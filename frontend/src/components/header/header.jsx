5885f5e4146d473c8fb4607d518708786951ccd4
StiggitS <142784487+StiggitS@users.noreply.github.com>
2024-01-22 10:56:39 +0100

    fixed header and nav

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,10 +1,12 @@
 import {React, useState} from 'react';
 import styles from './header.module.css'
+import logo from './../../../public/images/logo-vid.png'
+import burgerIcon from './../../../public/images/burger-icon.svg'
 
 
 function Header(props) {
-
-	const [isNavVisible, setNavVisibility] = useState(true);
+	//hvis useState default er true så loader websiden med nav menuen åben!
+	const [isNavVisible, setNavVisibility] = useState(false);
 
 
 	const toggleNav = () => {
@@ -17,7 +19,7 @@ function Header(props) {
 			<nav>
 				
 			<div className={styles.topnav}>
-  				<a href="#home" className={styles.active}>Logo</a>
+  				<a href="#home" className={styles.active}><img src={logo} alt="logo" /></a>
   				{/*<!-- Navigation links (hidden by default) -->*/}
   				<div id="myLinks" style={{ display: isNavVisible ? 'block' : 'none' }}>
     				<a href="#news">News</a>
@@ -26,7 +28,7 @@ function Header(props) {
   				</div>
   				{/*<!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->*/}
   				<a href="javascript:void(0);" className={styles.icon} onClick={toggleNav}>
-    				<i className="fa fa-bars"></i>
+				  <img src={burgerIcon} alt="" />
  				</a>
 			</div>
 
