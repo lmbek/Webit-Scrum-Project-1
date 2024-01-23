b80abef678a4046584ec16568560cfe2da879bc4
jonathan johansen <63632617+luffe141@users.noreply.github.com>
2024-01-23 12:37:08 +0100

    styling af realease

--- frontend/src/pages/releases/releases.jsx
+++ frontend/src/pages/releases/releases.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
 import Header from "../../components/header/header.jsx";
 import Footer from "../../components/footer/footer.jsx";
+import stlye from "./releases.module.css"
 
 function Releases() {
 	return (
 		<>
 			<Header></Header>
 			<main>
-				<article>
-					<h1>Releases</h1>
-					<section>
+			<h1>Releases</h1>
+				<article className={stlye.article}>
+					
+					<section className={stlye.releaseSection}>
 						<h2>0.4.0</h2>
 						<p>
 							<span>This is the release notes for version 0.4.0</span>
 						</p>
 						<a href={""}>Link</a>
 					</section>
-					<section>
+					<section className={stlye.releaseSection}>
 						<h2>0.3.0</h2>
 						<p>
 							<span>This is the release notes for version 0.3.0</span>
 						</p>
 						<a href={"kage"}>Link</a>
 					</section>
-					<section>
+					<section className={stlye.releaseSection}>
 						<h2>0.2.0</h2>
 						<p>
 							<span>This is the release notes for version 0.2.0</span>
 						</p>
 						<a href={""}>Link</a>
 					</section>
-					<section>
+					<section className={stlye.releaseSection}>
 						<h2>0.1.0</h2>
 						<p>
 							<span>This is the release notes for version 0.1.0</span>
