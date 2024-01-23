0ee9253f515b5fd23203a8c8a60763102191040b
Lars M Bek <lars@beksoft.dk>
2024-01-23 12:46:31 +0100

    Files Modified: * Updated release page

--- frontend/src/pages/releases/releases.jsx
+++ frontend/src/pages/releases/releases.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from "../../components/header/header.jsx";
 import Footer from "../../components/footer/footer.jsx";
-import stlye from "./releases.module.css"
+import style from "./releases.module.css"
 
 function Releases() {
 	return (
@@ -9,36 +9,39 @@ function Releases() {
 			<Header></Header>
 			<main>
 			<h1>Releases</h1>
-				<article className={stlye.article}>
+				<article className={style.article}>
 					
-					<section className={stlye.releaseSection}>
-						<h2>0.4.0</h2>
+					<section className={style.releaseSection}>
+						<h2>1.0.0</h2>
 						<p>
 							<span>This is the release notes for version 0.4.0</span>
 						</p>
-						<a href={""}>Link</a>
+						<a target={"_blank"} href={""}>Link</a>
 					</section>
-					<section className={stlye.releaseSection}>
+					<section className={style.releaseSection}>
 						<h2>0.3.0</h2>
 						<p>
 							<span>This is the release notes for version 0.3.0</span>
 						</p>
-						<a href={"kage"}>Link</a>
+						<a target={"_blank"} href={"https://github.com/UserWhy123/Scrum-Project-1/releases/tag/v0.3.0"}>Link</a>
 					</section>
-					<section className={stlye.releaseSection}>
+					<section className={style.releaseSection}>
 						<h2>0.2.0</h2>
 						<p>
 							<span>This is the release notes for version 0.2.0</span>
 						</p>
-						<a href={""}>Link</a>
+						<a target={"_blank"} href={"https://github.com/UserWhy123/Scrum-Project-1/releases/tag/v0.2.0"}>Link</a>
 					</section>
-					<section className={stlye.releaseSection}>
+					<section className={style.releaseSection}>
 						<h2>0.1.0</h2>
 						<p>
 							<span>This is the release notes for version 0.1.0</span>
 						</p>
-						<a href={""}>Link</a>
+						<a target={"_blank"} href={"https://github.com/UserWhy123/Scrum-Project-1/releases/tag/v0.1.0"}>Link</a>
 					</section>
+					<div className={style.seeAll}>
+						<a target={"_blank"} href={"https://github.com/UserWhy123/Scrum-Project-1/releases"}>See all releases</a>
+					</div>
 				</article>
 			</main>
 			<Footer></Footer>
