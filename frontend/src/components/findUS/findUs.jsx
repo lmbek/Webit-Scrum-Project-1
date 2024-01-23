8ed9889ef16898c0b418e0cd3303058a63c49158
hello <thismyemail@consultant.com>
2024-01-23 10:24:23 +0100

    findUs.jsx: modified * Jeg har tilføjet en map, for at vise hvor kunder kan finde os.

---
+++ frontend/src/components/findUS/findUs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import style from "./findUs.module.css"
+
+
+
+
+function FindUs() {
+	return (
+        
+		<div className={style.Map}>
+		<address className={style.Adresse}>
+            <h1>Find Us</h1>
+            <iframe className={style.googleMap} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2207.657929345504!2d10.887433999999999!3d56.4047207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464dd5b14f421121%3A0x7de336d6b4d8265c!2sYdesvej%204%2C%208500%20Gren%C3%A5!5e0!3m2!1sen!2sdk!4v1706000941107!5m2!1sen!2sdk"></iframe>
+        </address>
+		</div>
+	);
+}
+
+export default FindUs;
+
+
