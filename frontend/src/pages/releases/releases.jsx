5395938e99407ba34de57f9ddc421abb87548c17
Lars M Bek <lars@beksoft.dk>
2024-01-23 12:07:30 +0100

    releases.jsx: Modified * Added layout to page

--- frontend/src/pages/releases/releases.jsx
+++ frontend/src/pages/releases/releases.jsx
@@ -1,38 +1,46 @@
 import React from 'react';
+import Header from "../../components/header/header.jsx";
+import Footer from "../../components/footer/footer.jsx";
 
 function Releases() {
 	return (
-		<article>
-			<h1>Releases</h1>
-			<section>
-				<h2>0.4.0</h2>
-				<p>
-					<span>This is the release notes for version 0.4.0</span>
-				</p>
-				<a href={""}>Link</a>
-			</section>
-			<section>
-				<h2>0.3.0</h2>
-				<p>
-					<span>This is the release notes for version 0.3.0</span>
-				</p>
-				<a href={""}>Link</a>
-			</section>
-			<section>
-				<h2>0.2.0</h2>
-				<p>
-					<span>This is the release notes for version 0.2.0</span>
-				</p>
-				<a href={""}>Link</a>
-			</section>
-			<section>
-				<h2>0.1.0</h2>
-				<p>
-					<span>This is the release notes for version 0.1.0</span>
-				</p>
-				<a href={""}>Link</a>
-			</section>
-		</article>
+		<>
+			<Header></Header>
+			<main>
+				<article>
+					<h1>Releases</h1>
+					<section>
+						<h2>0.4.0</h2>
+						<p>
+							<span>This is the release notes for version 0.4.0</span>
+						</p>
+						<a href={""}>Link</a>
+					</section>
+					<section>
+						<h2>0.3.0</h2>
+						<p>
+							<span>This is the release notes for version 0.3.0</span>
+						</p>
+						<a href={"kage"}>Link</a>
+					</section>
+					<section>
+						<h2>0.2.0</h2>
+						<p>
+							<span>This is the release notes for version 0.2.0</span>
+						</p>
+						<a href={""}>Link</a>
+					</section>
+					<section>
+						<h2>0.1.0</h2>
+						<p>
+							<span>This is the release notes for version 0.1.0</span>
+						</p>
+						<a href={""}>Link</a>
+					</section>
+				</article>
+			</main>
+			<Footer></Footer>
+		</>
 	);
 }
 
