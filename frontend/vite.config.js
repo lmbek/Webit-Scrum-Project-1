59d6cafbecbe76c9ac48fd681a5c08d81ba718e8
Lars M Bek <lars@beksoft.dk>
2024-01-23 11:21:50 +0100

    Files Modified * Overall changes to everything

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteHtmlPlugin from 'vite-plugin-html';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteHtmlPlugin()],
 })
+
+
+
