ec6d42e43c9a57b6e423fb234775bbe06d50dd34
Lars M Bek <lars@beksoft.dk>
2024-01-23 12:00:46 +0100

    Files Added

---
+++ frontend/src/pages/releases/releases.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Releases() {
+	return (
+		<article>
+			<h1>Releases</h1>
+			<section>
+				<h2>0.4.0</h2>
+				<p>
+					<span>This is the release notes for version 0.4.0</span>
+				</p>
+				<a href={link}>Link</a>
+			</section>
+			<section>
+				<h2>0.3.0</h2>
+				<p>
+					<span>This is the release notes for version 0.3.0</span>
+				</p>
+				<a href={link}>Link</a>
+			</section>
+			<section>
+				<h2>0.2.0</h2>
+				<p>
+					<span>This is the release notes for version 0.2.0</span>
+				</p>
+				<a href={link}>Link</a>
+			</section>
+			<section>
+				<h2>0.1.0</h2>
+				<p>
+					<span>This is the release notes for version 0.1.0</span>
+				</p>
+				<a href={link}>Link</a>
+			</section>
+		</article>
+	);
+}
+
+export default Releases;
